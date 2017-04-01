583086b5ddeeaad66d2048e3db62427ef9a55029
priyanka <joshipriyanka193@gmail.com>
2017-03-31 20:24:48 -0400

    s5

--- saved.php
+++ saved.php
@@ -13,16 +13,17 @@
 		<h3 class="center">Your jobs are successfuly saved!</h3>
 
 <?php
-	//$user_id = $_GET["id"];
-	//$dbh = pg_connect("host='ec2-54-243-185-132.compute-1.amazonaws.com' dbname='d2ftjp5a24rakj' user='qqyirgarmsgczz' password='ae82f9bb4e7403bb24d558a33481c09dcad90e4520a9581cde72547a9a3063ca'");
+	$user_id = $_GET["id"];
+	$dbh = pg_connect("host='ec2-54-243-185-132.compute-1.amazonaws.com' dbname='d2ftjp5a24rakj' user='qqyirgarmsgczz' password='ae82f9bb4e7403bb24d558a33481c09dcad90e4520a9581cde72547a9a3063ca'");
 	//$sql_joblist = "SELECT * FROM JOB_SEARCH_RESULTS WHERE participant_id='".$user_id."'"; //'O8NnynqQqP1AG6B0jaKL'";//
 	
 	//$sql_participant = "SELECT * FROM PARTICIPANT WHERE participant_id='".$user_id."'"; //'O8NnynqQqP1AG6B0jaKL'";//
 	
-	//$sql_update_saved = "UPDATE JOB_SEARCH_RESULTS SET saved_job='true' WHERE job_search_id IN ()";
-	
 	$jobIds = $_POST["jobIds"];
-	echo implode(', ', $jobIds);
+	
+	$sql_update_saved = "UPDATE JOB_SEARCH_RESULTS SET saved_job='true' WHERE job_search_id IN (".echo implode(', ', $jobIds).")";
+	
+	echo $sql_update_saved;
     
 	
 ?>
